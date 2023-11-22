8facc465f9d4ddc9b53f467aa72c3d03e02673da
Fabio Roman <romanfabio@pm.me>
2023-11-22 14:57:52 +0100

    docs: add documentation of SimpleHttpRequestBuilder and SimpleHttpResponse

--- Void.SimpleHttp/SimpleHttpRequestBuilder.cs
+++ Void.SimpleHttp/SimpleHttpRequestBuilder.cs
@@ -7,67 +7,99 @@ using System.Text.Json;
 
 namespace Void.SimpleHttp
 {
+    /// <summary>Represents a simple builder to facilitate the creation of <see cref="HttpRequestMessage"/> instances.</summary>
     public class SimpleHttpRequestBuilder
     {
         private readonly HttpRequestMessage _request;
 
+        /// <summary>Initializes a new instance of the <see cref="SimpleHttpRequestBuilder"/> class.</summary>
         public SimpleHttpRequestBuilder()
         {
             _request = new HttpRequestMessage();
         }
 
+        /// <summary>Initializes a new instance of the <see cref="SimpleHttpRequestBuilder" /> class with an HTTP method and a request <see cref="T:System.Uri" />.</summary>
+        /// <param name="method">The HTTP method.</param>
+        /// <param name="requestUri">A string that represents the request  <see cref="T:System.Uri" />.</param>
         public SimpleHttpRequestBuilder(HttpMethod method, string requestUri)
         {
             _request = new HttpRequestMessage(method, requestUri);
         }
         
+        /// <summary>Initializes a new instance of the <see cref="SimpleHttpRequestBuilder"/> class with an HTTP method and a request <see cref="T:System.Uri" />.</summary>
+        /// <param name="method">The HTTP method.</param>
+        /// <param name="requestUri">The <see cref="T:System.Uri" /> to request.</param>
         public SimpleHttpRequestBuilder(HttpMethod method, Uri requestUri)
         {
             _request = new HttpRequestMessage(method, requestUri);
         }
         
+        /// <summary>Sets the HTTP method used by the HTTP request message.</summary>
+        /// <param name="method">The HTTP method.</param>
         public SimpleHttpRequestBuilder WithMethod(HttpMethod method)
         {
             _request.Method = method;
             return this;
         }
 
+        /// <summary>Sets the request <see cref="T:System.Uri"/> used by the HTTP request message.</summary>
+        /// <param name="requestUri">The <see cref="T:System.Uri"/> to request.</param>
         public SimpleHttpRequestBuilder WithUri(Uri requestUri)
         {
             _request.RequestUri = requestUri;
             return this;
         }
 
+        /// <summary>Sets the request <see cref="T:System.Uri"/> used by the HTTP request message.</summary>
+        /// <param name="requestUri">A string that represents the request <see cref="T:System.Uri"/>.</param>
         public SimpleHttpRequestBuilder WithUri(string requestUri)
         {
             _request.RequestUri = new Uri(requestUri);
             return this;
         }
 
+        /// <summary>Sets the Bearer token used by the HTTP request message.</summary>
+        /// <param name="token">The token.</param>
+        /// <param name="prefix">The prefix used for the token. The default is "Bearer".</param>
         public SimpleHttpRequestBuilder WithBearerToken(string token, string prefix = "Bearer")
         {
             _request.Headers.Authorization = new AuthenticationHeaderValue(prefix, token);
             return this;
         }
 
+        /// <summary>
+        /// Serialize the value and uses it as JSON body for the HTTP request message.
+        /// The body is configured with UTF-8 encoding and "application/json" media type.
+        /// </summary>
+        /// <param name="value">The value to be serialized.</param>
+        /// <param name="options">Options to control the serialization behavior.</param>
+        /// <typeparam name="T">The type of the value to serialize.</typeparam>
         public SimpleHttpRequestBuilder WithJsonBody<T>(T value, JsonSerializerOptions? options = null)
         {
             _request.Content = new StringContent(JsonSerializer.Serialize(value, options), Encoding.UTF8, "application/json");
             return this;
         }
 
+        /// <summary>Adds a single-value header to the HTTP request message.</summary>
+        /// <param name="name">The header to add to the HTTP request message.</param>
+        /// <param name="value">The content of the header.</param>
         public SimpleHttpRequestBuilder WithHeader(string name, string value)
         {
             _request.Headers.Add(name, value);
             return this;
         }
 
+        /// <summary>Adds a multi-value header to the HTTP request message.</summary>
+        /// <param name="name">The header to add to the HTTP request message.</param>
+        /// <param name="values">The values of the header.</param>
         public SimpleHttpRequestBuilder WithHeader(string name, IEnumerable<string> values)
         {
             _request.Headers.Add(name, values);
             return this;
         }
 
+        /// <summary>Adds multiple single-value headers to the HTTP request message.</summary>
+        /// <param name="headers">The headers to add to the HTTP request message.</param>
         public SimpleHttpRequestBuilder WithHeaders(IDictionary<string, string> headers)
         {
             foreach (var (name, value) in headers)
@@ -78,6 +110,8 @@ namespace Void.SimpleHttp
             return this;
         }
 
+        /// <summary>Adds multiple multi-value headers to the HTTP request message.</summary>
+        /// <param name="headers">The headers to add to the HTTP request message.</param>
         public SimpleHttpRequestBuilder WithHeaders(IDictionary<string, IEnumerable<string>> headers)
         {
             foreach (var (name, value) in headers)
@@ -88,6 +122,8 @@ namespace Void.SimpleHttp
             return this;
         }
 
+        /// <summary>Returns the built instance of <see cref="HttpRequestMessage"/>.</summary>
+        /// <returns>The built instance of <see cref="HttpRequestMessage"/>.</returns>
         public HttpRequestMessage Build()
         {
             return _request;
--- Void.SimpleHttp/SimpleHttpResponse.cs
+++ Void.SimpleHttp/SimpleHttpResponse.cs
@@ -8,32 +8,46 @@ using System.Threading.Tasks;
 
 namespace Void.SimpleHttp
 {
+    /// <summary>Represents a simplified and thin wrapper around <see cref="HttpResponseMessage"/> instances.</summary>
     public class SimpleHttpResponse
     {
         private readonly HttpResponseMessage _response;
 
+        /// <summary>Initializes a new instance of the <see cref="SimpleHttpResponse"/> class.</summary>
+        /// <param name="response">The <see cref="HttpResponseMessage"/> instance to manage.</param>
         public SimpleHttpResponse(HttpResponseMessage response)
         {
             _response = response;
         }
         
+        /// <summary>Gets the status code of the HTTP response.</summary>
         public HttpStatusCode StatusCode => _response.StatusCode;
 
+        /// <summary>Gets a value that indicates if the HTTP response was successful.</summary>
         public bool IsSuccessStatusCode => _response.IsSuccessStatusCode;
 
+        /// <summary>Gets the reason phrase which typically is sent by servers together with the status code.</summary>
         public string ReasonPhrase => _response.ReasonPhrase;
 
+        /// <summary>Returns the underlying <see cref="HttpResponseMessage"/> instance.</summary>
         public HttpResponseMessage GetRawResponse()
         {
             return _response;
         }
 
+        /// <summary>Throws an exception if the <see cref="SimpleHttpResponse.IsSuccessStatusCode" /> property for the HTTP response is <see langword="false" />.</summary>
+        /// <exception cref="T:System.Net.Http.HttpRequestException">The HTTP response is unsuccessful.</exception>
         public SimpleHttpResponse EnsureSuccessStatusCode()
         {
             _response.EnsureSuccessStatusCode();
             return this;
         }
 
+        /// <summary>Reads the HTTP JSON content and return the deserialized object.</summary>
+        /// <param name="options">Options to control the deserialization behavior.</param>
+        /// <param name="cancellationToken">The cancellation token to cancel the operation.</param>
+        /// <typeparam name="TValue">The type to deserialize the JSON content into.</typeparam>
+        /// <returns>A TValue representation of the JSON content.</returns>
         public async Task<TValue> DeserializeJsonContentAsync<TValue>(JsonSerializerOptions? options = null, CancellationToken cancellationToken = default)
         {
             return await JsonSerializer.DeserializeAsync<TValue>(
@@ -43,16 +57,29 @@ namespace Void.SimpleHttp
             );
         }
 
+        /// <summary>Returns the first header value for a specified header stored in the <see cref="T:System.Net.Http.Headers.HttpHeaders" /> collection.</summary>
+        /// <param name="name">The specified header to return value for.</param>
+        /// <exception cref="T:System.InvalidOperationException">The header cannot be found.</exception>
+        /// <returns>The first header value or <see langword="null"/> if the header has no value</returns>
         public string GetHeaderValue(string name)
         {
             return GetHeaderValues(name).FirstOrDefault();
         }
 
+        /// <summary>Returns all header values for a specified header stored in the <see cref="T:System.Net.Http.Headers.HttpHeaders" /> collection.</summary>
+        /// <param name="name">The specified header to return values for.</param>
+        /// <exception cref="T:System.InvalidOperationException">The header cannot be found.</exception>
+        /// <returns>An array of header strings.</returns>
         public IEnumerable<string> GetHeaderValues(string name)
         {
             return _response.Headers.GetValues(name);
         }
 
+        /// <summary>Return if a specified header and specified values are stored in the <see cref="T:System.Net.Http.Headers.HttpHeaders" /> collection.</summary>
+        /// <param name="name">The specified header.</param>
+        /// <param name="values">The specified header values.</param>
+        /// <returns>
+        /// <see langword="true" /> if the specified header <paramref name="name" /> and <see langword="values" /> are stored in the collection; otherwise <see langword="false" />.</returns>
         public bool TryGetHeaderValues(string name, out IEnumerable<string> values)
         {
             return _response.Headers.TryGetValues(name, out values);
